2758cbdb404209c8f03cdbb17705921face83c8a
SemihParlayan <semih.parlayan@hotmail.com>
2023-08-12 12:57:08 +0200

    Upgrade to 2022

--- Assets/Rewired/Examples/CustomControllersTilt/Scripts/CustomControllersTiltDemo.cs
+++ Assets/Rewired/Examples/CustomControllersTilt/Scripts/CustomControllersTiltDemo.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:301dcc2c860a649076e32a1a3f879e43529929f5f3405a3ec7ca290538df86ff
-size 2309
+oid sha256:30d02ef7f172d6352c671ee4fb5874f5cd888e6fb77159d78faa165a24f06559
+size 2313
--- Assets/Rewired/Examples/CustomControllersTouch/Scripts/CustomControllerDemo.cs
+++ Assets/Rewired/Examples/CustomControllersTouch/Scripts/CustomControllerDemo.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d46abd885d584a7ed86dbdac78edfaf92c56e25819289710be3cbc61c91716b3
-size 7545
+oid sha256:3fa00e6626994f2a27634cd999871954d62d694af5a98e6e2a88bda7d330b4a3
+size 7553
